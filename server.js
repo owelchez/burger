ad2c48e1bf532acd6668c9909ac9df57887b0473
Orvin Welchez <owelchez@CRESTRON.CRESTRON.COM>
2016-08-09 09:36:59 -0400

    Update

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var bodyParser = require('body-parser');
 var method_override = require('method-override');
+var mysql = require('mysql');
 
 var app = express(); // DUH!
 
@@ -14,14 +15,21 @@ app.set('view engine', 'handlebars');
 //Serve static content for the app from the "public" directory in the application directory.
 app.use(express.static(__dirname + '/public'));
 
+    /*MySQL connection initialization*/
+var connection = mysql.createConnection({
+  host     : 'localhost',
+  user     : 'root',
+  password : '',
+  database : 'burgers_db'
+});
+
 //root get route
 app.get('/', function(req,res) {
-
     connection.query('SELECT * FROM events;', function(err, data) {
       if (err) throw err;
 
       //test it
-      //console.log('The solution is: ', data);
+      console.log('The solution is: ', data);
 
       //test it
       //res.send(data);
@@ -48,6 +56,15 @@ app.post('/create', function(req, res) {
     });
 });
 
+
+
+/********************************************/
+                /*MySQL Commands*/
+/********************************************/
+
+
+
+/********************************************/
 
 var PORT = 3000;
 app.listen(PORT);
